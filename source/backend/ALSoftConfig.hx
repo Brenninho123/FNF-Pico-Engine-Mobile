2eaba42bcbfd55d028954bc0651a0b47bce09bae
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-04-29 17:16:16 -0300

    Add files via upload

---
+++ source/backend/ALSoftConfig.hx
@@ -0,0 +1,28 @@
+package backend;
+
+import haxe.io.Path;
+
+/*
+A class that simply points OpenALSoft to a custom configuration file when the game starts up.
+The config overrides a few global OpenALSoft settings with the aim of improving audio quality on desktop targets.
+*/
+@:keep class ALSoftConfig
+{
+	#if desktop
+	static function __init__():Void
+	{
+		var origin:String = #if hl Sys.getCwd() #else Sys.programPath() #end;
+
+		var configPath:String = Path.directory(Path.withoutExtension(origin));
+		#if windows
+		configPath += "/alsoft.ini";
+		#else
+		configPath += "/content/plugins/audio-config.ini";
+		#else
+		configPath += "/content/plugins/alsoft.DAT";
+		#end
+
+		Sys.putEnv("ALSOFT_CONF", configPath);
+	}
+	#end
+}

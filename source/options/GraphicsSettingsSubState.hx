94a8bb1e0ed844e4f86fda2a28431b068a7fdad5
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-04-29 17:19:31 -0300

    Add files via upload

---
+++ source/options/GraphicsSettingsSubState.hx
@@ -0,0 +1,98 @@
+package options;
+
+import objects.Character;
+
+class GraphicsSettingsSubState extends BaseOptionsMenu
+{
+	var antialiasingOption:Int;
+	var boyfriend:Character = null;
+	public function new()
+	{
+		title = Language.getPhrase('graphics_menu', 'Graphics Settings');
+		rpcTitle = 'Graphics Settings Menu';
+
+		boyfriend = new Character(840, 170, 'pico', true);
+		boyfriend.setGraphicSize(Std.int(boyfriend.width * 0.9));
+		boyfriend.updateHitbox();
+		boyfriend.dance();
+		boyfriend.animation.finishCallback = function (name:String) boyfriend.dance();
+		boyfriend.visible = false;
+
+		var option:Option = new Option('Quality',
+			'If checked, disables some background details,\ndecreases loading times and improves performance.', //Description
+			'Quality',
+			STRING,
+			['Low', 'High']);
+		addOption(option);
+
+		var option:Option = new Option('Anti-Aliasing',
+			'If unchecked, disables anti-aliasing, increases performance\nat the cost of sharper visuals.',
+			'antialiasing',
+			BOOL);
+		option.onChange = onChangeAntiAliasing; //Changing onChange is only needed if you want to make a special interaction after it changes the value
+		addOption(option);
+		antialiasingOption = optionsArray.length-1;
+
+		var option:Option = new Option('Shaders', //Name
+			"If unchecked, disables shaders.\nIt's used for some visual effects, and also CPU intensive for weaker PCs.", //Description
+			'shaders',
+			BOOL);
+		addOption(option);
+
+		var option:Option = new Option('GPU Caching', //Name
+			"If checked, allows the GPU to be used for caching textures, decreasing RAM usage.\nDon't turn this on if you have a shitty Graphics Card.", //Description
+			'cacheOnGPU',
+			BOOL);
+		addOption(option);
+		
+	#if !html5
+		var option:Option = new Option('Framerate',
+			"Pretty self explanatory, isn't it?",
+			'framerate',
+			INT);
+		addOption(option);
+
+		final refreshRate:Int = FlxG.stage.application.window.displayMode.refreshRate;
+		option.minValue = 30;
+		option.maxValue = 240;
+		option.maxValue = 999;
+		option.defaultValue = Std.int(FlxMath.bound(refreshRate, option.minValue, option.maxValue));
+		option.displayFormat = '%v FPS';
+		option.onChange = onChangeFramerate;
+		#end
+
+		super();
+		insert(1, boyfriend);
+	}
+
+	function onChangeAntiAliasing()
+	{
+		for (sprite in members)
+		{
+			var sprite:FlxSprite = cast sprite;
+			if(sprite != null && (sprite is FlxSprite) && !(sprite is FlxText)) {
+				sprite.antialiasing = ClientPrefs.data.antialiasing;
+			}
+		}
+	}
+
+	function onChangeFramerate()
+	{
+		if(ClientPrefs.data.framerate > FlxG.drawFramerate)
+		{
+			FlxG.updateFramerate = ClientPrefs.data.framerate;
+			FlxG.drawFramerate = ClientPrefs.data.framerate;
+		}
+		else
+		{
+			FlxG.drawFramerate = ClientPrefs.data.framerate;
+			FlxG.updateFramerate = ClientPrefs.data.framerate;
+		}
+	}
+
+	override function changeSelection(change:Int = 0)
+	{
+		super.changeSelection(change);
+		boyfriend.visible = (antialiasingOption == curSelected);
+	}
+}

20e9cb6753a56d6ef3c3d97676997e2c467f463a
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-05-10 16:32:04 -0300

    Add files via upload

---
+++ source/funkin/utils/vslice/VsliceOptions.hx
@@ -0,0 +1,21 @@
+package funkin.utils.vslice;
+
+class VsliceOptions 
+{
+    public static var ANTIALIASING(get,never):Bool;    
+    public static function get_ANTIALIASING():Bool {
+        return ClientPrefs.data.antialiasing;
+    }
+    public static var IS_LOW_QUALITY(get,never):Bool;    
+    public static function get_IS_LOW_QUALITY():Bool {
+        return ClientPrefs.isLowQuality;
+    }
+    public static var SHADERS(get,never):Bool;    
+    public static function get_SHADERS():Bool {
+        return ClientPrefs.data.shaders;
+    }
+    public static var FLASHBANG(get,never):Bool;    
+    public static function get_FLASHBANG():Bool {
+        return ClientPrefs.data.flashing;
+    }
+}
